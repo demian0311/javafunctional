9b055e192f0ad5bcf2ee27b7b8e6d24e3e373173
Demian Neidetcher <demian0311@gmail.com>
2014-01-18 17:41:09 -0600

    before coming up with a good example

--- src/test/java/javafunctional/java8/examples/Lambdas.java
+++ src/test/java/javafunctional/java8/examples/Lambdas.java
@@ -2,6 +2,8 @@ package javafunctional.java8.examples;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -12,28 +14,46 @@ import static org.junit.Assert.assertTrue;
 
 /**
  * Example usage for some of the things in java.util.function.
+ *
+ * Figure out a use case that chains all of these functions together.
  */
 public class Lambdas {
+
+    private List<Stock> portfolio = Arrays.asList(
+            new Stock("GOOG", 400, 759),
+            new Stock("AAPL", 5, 100),
+            new Stock("MSFT", 5, 19),
+            new Stock("CSCO", 5, 84));
+
     @Test public void predicate() {
         Predicate<Integer> divBy3 = (arg) -> arg % 3 == 0? true: false;
         assertEquals(true, divBy3.test(9));
     }
 
+    Function<String, String> reverse = (arg) -> (new StringBuffer(arg)).reverse().toString();
+
     @Test public void function(){
-        Function<String, String> reverse = (arg) -> (new StringBuffer(arg)).reverse().toString();
         assertEquals("olleH", reverse.apply("Hello"));
     }
 
-    @Test public void supplier() {
-        Supplier<String> headsOrTails = () -> (System.currentTimeMillis() % 2 == 0) ? "HEADS" : "TAILS";
+    Supplier<String> headsOrTails = () -> (System.currentTimeMillis() % 2 == 0) ? "HEADS" : "TAILS";
 
+    @Test public void supplier() {
         String result = headsOrTails.get();
         assertTrue(result.equals("HEADS") || result.equals("TAILS"));
     }
 
+    Consumer<String> println = (arg) -> System.out.println(arg);
+
+    /**
+     * This interface is meant for side-effects.
+     */
     @Test public void consumer() {
-        Consumer<String> println = (arg) -> System.out.println(arg);
         println.accept("Hello World");
+    }
+
+    @Test public void allTogether(){
+        //reverse.
     }
 
 }
