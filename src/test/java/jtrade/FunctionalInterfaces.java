a2106ccdd548d6b282334c4b57eff112dc85e9f2
Demian Neidetcher <demian0311@gmail.com>
2014-02-02 14:29:26 -0600

    organize functional interfaces more

--- src/test/java/jtrade/FunctionalInterfaces.java
+++ src/test/java/jtrade/FunctionalInterfaces.java
@@ -12,40 +12,49 @@ import static org.junit.Assert.assertTrue;
 
 /**
  * Demonstrate using the functional interfaces in java.util.function.
- * And what interfaces correspond to what functions.
+ * A lot of times you won't need to declare functions such that you'd want to have
+ * them typed as an interface but sometimes you might.  Here are some examples of
+ * doing that.  You can also create your own Single Abstract Method Interfaces and
+ * use them.
  */
 public class FunctionalInterfaces {
 
+    /**
+     * Predicate takes an argument of a type and always returns a Boolean.
+     * This example usage is equivalent to declaring a Function<Stock, Boolean>.
+     */
     @Test public void predicate() {
         Predicate<Stock> valueOver1000 = s -> s.getValue() > 1000;
         assertEquals(false, valueOver1000.test(new Stock("FOO", 11.0, 20)));
     }
 
-    // TODO: show Function
-    //Function<Stock, Double> findValue = (Stock s) -> s.getPrice() * s.getQuantity();
-
-    /*
+    /**
+     * Function needs the type it takes and the type it returns.
+     */
     @Test public void function() {
+        Function<Stock, Double> findValue = (Stock s) -> s.getPrice() * s.getQuantity();
         assertEquals(new Double("30"), findValue.apply(new Stock("GOOG", 3.0, 10)));
-    }*/
+    }
 
-    Supplier<Boolean> tradingWindowOpen = () -> true;
-
+    /**
+     * Supplier takes nothing and returns the type you specify.
+     */
     @Test public void supplier() {
+        Supplier<Boolean> tradingWindowOpen = () -> true;
         assertTrue(tradingWindowOpen.get());
     }
 
-    Consumer<Stock> sell = (arg) -> System.out.println("\tSELL ORDER: " + arg);
-
+    /**
+     * Consumer takes the type you specify and returns nothing.  The implication here
+     * is that the Consumer is hiding some side-effect.  Maybe it's dealing with IO or
+     * doing a non-read operation to another system.
+     */
     @Test public void consumer() {
+        Consumer<Stock> sell = (arg) -> System.out.println("\tSELL ORDER: " + arg);
         sell.accept(new Stock("FOO", 3.0, 12));
     }
 
-    // TODO-DLN: show a couple of the functions that work with primitives.
 }
-
+// TODO-DLN: show a couple of the functions that work with primitives.
 // TODO: show an interface that doesn't compile when you try to add multiple abstract methods
-
 // TODO: show that default methods are not counted as abstract methods
-
-
